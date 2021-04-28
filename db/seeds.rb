d0b8f603e4f3e95332822152073f83f1c32222d7
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-04-28 05:52:53 +0400

    Change name to title

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
 
-  Movie.create(name:'Apocalipto',rating:'2',total_gross:34.3)
-  Movie.create(name:'Animal_farm',rating:'24',total_gross:3.3)
+  Movie.create(title:'Apocalipto',rating:'2',total_gross:34.3)
+  Movie.create(title:'Animal_farm',rating:'24',total_gross:3.3)
