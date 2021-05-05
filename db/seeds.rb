ce4acac8b0e8f7ab2ab154c283a32114eb691af7
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-05-06 00:35:48 +0400

    Add prepared images to the project

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,2 @@
-
-  Movie.create(title:'Apocalipto',rating:'2',total_gross:34.3)
-  Movie.create(title:'Animal_farm',rating:'24',total_gross:3.3)
+Movie.create(title: 'Apocalipto', rating: '2', total_gross: 34.3)
+Movie.create(title: 'Animal_farm', rating: '24', total_gross: 3.3)
