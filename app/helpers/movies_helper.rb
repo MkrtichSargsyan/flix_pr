d09d752d142539e0786c8bd6a6346e3df595b1c4
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-05-17 23:24:13 +0400

    add helper methods

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,9 +1,13 @@
 module MoviesHelper
   def total_gross(movie)
-    if movie.total_gross < 225_000_000
+    if movie.flop?
       'Flop!'
     else
       number_to_currency(movie.total_gross, precision: 0)
     end
+  end
+
+  def year_of(movie)
+    movie.released_on.strftime('%Y')
   end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,7 @@
 class Movie < ApplicationRecord
+
+  def flop?
+    total_gross.blank? || total_gross < 225_000_000
+  end
+  
 end
