d600a3cf5cc1f61e6fcd018dc3a8980ede91bfcb
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-05-31 11:46:07 +0400

    add show method for 1 movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,4 +3,9 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+
+  def show
+    @movie = Movie.find(1)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get 'movies' => 'movies#index'
+  get 'movies/1' => 'movies#show'
 end
