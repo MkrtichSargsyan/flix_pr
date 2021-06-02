3f9ec14481db6a7cff5ff47c33d383e6064a1890
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-06-02 15:49:58 +0400

    update movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,19 @@
 class MoviesController < ApplicationController
-
   def index
     @movies = Movie.all
   end
 
   def show
-    @movie = Movie.find(params[:id]) 
+    @movie = Movie.find(params[:id])
   end
 
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    movie_params = params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
+    @movie.update(movie_params)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   get 'movies' => 'movies#index'
   get 'movies/:id' => 'movies#show', as: 'movie'
+  get 'movies/:id/edit' => 'movies#edit', as: 'edit_movie'
+  patch "movies/:id" => "movies#update"
 end
