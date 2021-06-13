f2e9990a3fe81e67cccc267201282625c8555c43
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-06-13 07:22:10 +0400

    add released method into movie model,to show only released movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+    @movies = Movie.released
   end
 
   def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,9 @@
 class Movie < ApplicationRecord
+  def self.released
+    where('released_on < ?', Time.now).order('released_on desc')
+  end
 
   def flop?
     total_gross.blank? || total_gross < 225_000_000
   end
-  
 end
