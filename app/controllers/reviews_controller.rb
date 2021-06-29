18c3633b143b4448f0f3397a6bf984e5f346ef28
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-06-29 05:52:59 +0400

    Add functionality for making new review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,32 @@
 class ReviewsController < ApplicationController
+
+  before_action :set_movie
+
   def index
-    @movie = Movie.find(params[:movie_id])
     @reviews = @movie.reviews
+  end
+
+  def new
+    @review = @movie.reviews.new
+  end
+
+  def create
+    @review = @movie.reviews.new(review_params)
+
+    if @review.save
+      redirect_to movie_reviews_path(@movie), notice: 'Thanks for your review!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:name, :comment, :stars)
+  end
+
+  def set_movie
+    @movie = Movie.find(params[:movie_id])
   end
 end
